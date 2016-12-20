6f3917c32532f8e707201b5c38ac546881a0e214
Kazak Dmitry <kazak.dmitry1989@mail.ru>
2016-12-20 07:57:30 +0200

    work version

--- public/js/controller/mainCtrl.js
+++ public/js/controller/mainCtrl.js
@@ -2,6 +2,20 @@ angular.module('mainModule')
 	.controller("mainCtrl",function($scope,dataService){
 		$scope.dataMarkets = dataService.Markets;
 
+		var t = $scope.dataMarkets;
+
+		var data = [];
+
+		for(var key in t){
+			data.push(
+				t[key]
+				
+			);
+		}
+
+		$scope.lists = data;
+
+		$scope.markets = [1,2,3,4,5]
 		$scope.sortableOptions = {
     
 		    update: function(e, ui) {
